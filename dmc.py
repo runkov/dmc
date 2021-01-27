aa98ca5a5f31bff1fe52fc9b29eb4f14d3bf90fc
Runkoff <d.v.runkov@roek.ryazan.ru>
2021-01-27 16:50:19 +0300

    sprite sheet

--- dmc.py
+++ dmc.py
@@ -20,6 +20,17 @@ def load_image(name, color_key=None):
     return image
 
 
+#  список нарезаных спрайтов
+def load_frames(name, columns, rows):
+    im = load_image(name)
+    result = []
+    for j in range(rows):
+        for i in range(columns):
+            frame_location = (108 * i, 149 * j)
+            result.append(im.surface(Rect(frame_location, (108, 149))))
+    return result
+
+
 class Player:
     def __init__(self, x, y, speed=(0, 0)):
         self.x = x
@@ -27,6 +38,7 @@ class Player:
         self.bounds = Rect(x, y, 50, 50)
         self.speed = speed  # (dx, dy)
         self.image = load_image('hero01.jpeg', -1)
+        self.frames = load_frames('sprite-sheet-walking-girl.png', 6, 5)
 
     def handle_events(self, keys, fps):
         dx = 0
